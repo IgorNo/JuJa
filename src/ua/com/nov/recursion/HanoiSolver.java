1f7a933842853ea107bee115977b88b28ce5cd2f
IgorNo <novikof1@inbox.ru>
2016-09-15 18:57:26 +0300

    Juja tower of Hanoi

---
+++ src/ua/com/nov/recursion/HanoiSolver.java
@@ -0,0 +1,28 @@
+package ua.com.nov.recursion;
+
+import java.util.Stack;
+
+public class HanoiSolver {
+    public static void exchange(Stack from, Stack help, Stack to, int count) {
+        if (count > 0) {
+            exchange(from,to,help,count-1); // перенести башню из n−1 диска на 2-й штырь
+            int biggest = (int) from.pop();
+            to.push(biggest); // переносим самый большой диск на 3-й штырь
+            exchange(help,from,to,count-1); // перенеси башню из n−1 диска на 3-й штырь
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> from = new Stack<>();
+        Stack<Integer> to = new Stack<>();
+        Stack<Integer> help = new Stack<>();
+
+        from.push(3);
+        from.push(2);
+        from.push(1);
+
+        System.out.println("print from = " + from);
+        exchange(from, help, to, from.size());
+        System.out.println("print to = "+ to);
+    }
+} 
